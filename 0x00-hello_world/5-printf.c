62e431b20a04da98ca19ca059eb91540ae3bdf24
Gilbert Benimana <bengil994.bg@gmail.com>
2023-03-16 05:11:29 -0700

    Writing C program

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+/* The function printing the string using printf */
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+/* Returning 0 for success */
+return (0);
+}
