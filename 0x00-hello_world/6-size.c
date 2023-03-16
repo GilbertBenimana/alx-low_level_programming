80cb6dbb2dec5ad8c6841276396fc6e05a4345ca
Gilbert Benimana <bengil994.bg@gmail.com>
2023-03-16 06:57:09 -0700

    Writing C program

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,11 +12,13 @@ int main(void)
 {
 char c = 'a';
 int n = 1;
+long int ln = 2;
+long long int lln = 3;
 float fn = 1.5;
 printf("Size of a char: %lu byte(s)\n", sizeof(c));
 printf("Size of an int: %lu byte(s)\n", sizeof(n));
-printf("Size of a long int: %lu byte(s)\n", sizeof((long) n));
-printf("Size of a long long int: %lu byte(s)\n", sizeof((long)(long) n));
+printf("Size of a long int: %lu byte(s)\n", sizeof(ln));
+printf("Size of a long long int: %lu byte(s)\n", sizeof(lln));
 printf("Size of a float: %lu byte(s)\n", sizeof(fn));
 /* Returning 0 for success */
 return (0);
