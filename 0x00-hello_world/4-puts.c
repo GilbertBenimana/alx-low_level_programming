63d8723afd443427338ae2a870d75c7f28e37b31
Gilbert Benimana <112483007+GilbertBenimana@users.noreply.github.com>
2023-03-16 13:49:09 +0200

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,15 @@
-/**
- *This program print a string using puts
- * printf is not allowed
-*/
 #include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 
 /* The function printing the string using puts */
 
 int main(void)
 {
-    
     puts("\"Programming is like building a multilingual puzzle");
     
     /* Returning 0 for success */
