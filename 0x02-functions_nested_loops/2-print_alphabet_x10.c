38824417351d1f1d229f9dcb09b16891ff7fee59
Gilbert Benimana <112483007+GilbertBenimana@users.noreply.github.com>
2023-03-21 23:28:53 +0200

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,13 +7,13 @@
  */
 void print_alphabet_x10(void)
 {
-	int letter;
-	int count = 0;
-	while (count < 10)
-	{    
-	for (letter = 'a'; letter <= 'z'; letter++)
-		_putchar(letter);
-	_putchar('\n');
-	count++;
+	char i;
+	int j;
+
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+			_putchar(i);
+		_putchar('\n');
 	}
 }
