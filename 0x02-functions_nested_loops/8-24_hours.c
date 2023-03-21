be92b0cb17822048a483fecea9b081f1fa72042f
Gilbert Benimana <112483007+GilbertBenimana@users.noreply.github.com>
2023-03-22 00:42:32 +0200

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,25 +1,24 @@
 #include "main.h"
+
 /**
- * print_sign - Print the sign of a number
- * @n: type int integer, can be negative or positive
- * Description: print +, 0, or - depending on number, along with return
- * Return: 1 if +, 0 if 0, and -1 if -
+ * jack_bauer - Check Main
+ * Description: function prints every minute in one day
+ * Return: Nothing
  */
-int print_sign(int n)
+void jack_bauer(void)
 {
-	if (n > 0)
+	int H, M;
+
+	for (H = 0; H < 24; H++)
 	{
-		_putchar('+');
-		return (1);
-	}
-	else if (n < 0)
-	{
-		_putchar('-');
-		return (-1);
-	}
-	else
-	{
-		_putchar('0');
-		return (0);
+		for (M = 0; M < 60; M++)
+		{
+			_putchar((H / 10) + '0');
+			_putchar((H % 10) + '0');
+			_putchar(':');
+			_putchar((M / 10) + '0');
+			_putchar((M % 10) + '0');
+			_putchar('\n');
+		}
 	}
 }
