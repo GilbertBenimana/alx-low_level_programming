d95f0658d3fb891d27813134f22cb05978aa6c91
Gilbert Benimana <112483007+GilbertBenimana@users.noreply.github.com>
2023-03-21 23:09:56 +0200

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,7 +4,10 @@
  * main - Entry point
  * Return: Always 1 (Success)
  */
-/*alphabet function that print out all letters*/
+/**
+ *alphabet function 
+ *that print out all letters
+ */
 void print_alphabet(void)
 {
 	int letter;
