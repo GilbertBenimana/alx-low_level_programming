7bcf92ab87b9f0d2219d40a44b3ef3bfdb880ce0
Gilbert Benimana <bengil994.bg@gmail.com>
2023-03-21 13:28:17 -0700

    Nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+#include "holberton.h"
+
+/**
+ * main - Entry point
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+char word[8] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i++)
+		_putchar(word[i]);
+	_putchar('\n');
+
+	return (0);
+}
