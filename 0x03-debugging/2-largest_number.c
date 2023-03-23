adeb323b7d82d8b3a97e8cdc9a72db67fee2837c
Gilbert Benimana <112483007+GilbertBenimana@users.noreply.github.com>
2023-03-23 11:18:31 +0200

    Update 2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,32 +10,32 @@
 
 int largest_number(int a, int b, int c)
 {
-    int largest;
+        int largest;
 
-    if (a >= b && b >= c)
-    {
-        largest = a;
-    }
-    else if (a >= c && c >= b)
-    {
-        largest = a;
-    }
-    else if (b >= a && a >= c)
-    {
-        largest = b;
-    }
-    else if (b >= c && c >= a)
-    {
-        largest = b;
-    }
-    else if (c >= a && a >= b)
-    {
-        largest = c;
-    }
-    else if (c >= b && b >= a)
-    {
-        largest = c;
-    }
+        if (a >= b && b >= c)
+        {
+            largest = a;
+        }
+        else if (a >= c && c >= b)
+        {
+            largest = a;
+        }
+        else if (b >= a && a >= c)
+        {
+            largest = b;
+        }
+        else if (b >= c && c >= a)
+        {
+            largest = b;
+        }
+        else if (c >= a && a >= b)
+        {
+            largest = c;
+        }
+        else if (c >= b && b >= a)
+        {
+            largest = c;
+        }
 
-    return (largest);
+        return (largest);
 }
