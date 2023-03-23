bfb3018061f81c44454e97e4491925d26d80e6f7
Gilbert Benimana <112483007+GilbertBenimana@users.noreply.github.com>
2023-03-23 15:56:45 +0200

    Update 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,17 +7,17 @@
  */
 void more_numbers(void)
 {
-	int i, j = 0;
+int i, j = 0;
 
-	for (; j < 10; j++)
-	{
-		for (i = 0; i <= 14; i++)
-		{
-			if (i > 9)
-				_putchar(i / 10 + '0');
+for (; j < 10; j++)
+{
+for (i = 0; i <= 14; i++)
+{
+if (i > 9)
+_putchar(i / 10 + '0');
 
-			_putchar(i % 10 + '0');
-		}
-		_putchar('\n');
-	}
+_putchar(i % 10 + '0');
+}
+_putchar('\n');
+}
 }
