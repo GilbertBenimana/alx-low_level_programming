48865028d2cba21860e3d641ec008b34fc280f27
Gilbert Benimana <112483007+GilbertBenimana@users.noreply.github.com>
2023-03-23 16:15:31 +0200

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,26 +1,27 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
- * more_numbers - print 10 times the numbers from 0 to 14
- * Description: Can only use _putchar 3 times
+ * more_numbers - print numbers
+ * Description: prints numbers from 0 to 14 10 times
+ * Return: No return
  */
+
 void more_numbers(void)
 {
-	int c, i;
-
-	c = 0;
-	while (c < 10)
-	{
-		i = 0;
-		while (i < 15)
-		{
-			if (i > 9)
-				_putchar(i / 10 + '0');
-
-			_putchar(i % 10 + '0');
-			i++;
-		}
-		c++;
-		_putchar('\n');
-	}
+int i;
+int j;
+while (i < 10)
+{
+for (j = 0; j <= 14; j++)
+{
+if (j > 9)
+{
+_putchar(j / 10 + '0');
 }
+_putchar(j%10 + '0'); 
+}
+_putchar('\n');
+i++;
+}
+}
+
