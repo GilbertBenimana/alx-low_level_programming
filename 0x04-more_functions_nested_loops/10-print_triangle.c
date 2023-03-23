5bf9d07aeb4f9f42511344a6522019deae0807ad
Gilbert Benimana <112483007+GilbertBenimana@users.noreply.github.com>
2023-03-23 20:54:36 +0200

    Update 10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -4,10 +4,10 @@
  * print_triangle - print a triangle aligned right, using '#'
  * @size: Size of triangle
  */
+
 void print_triangle(int size)
 {
 int c, i, j;
-
 c = 0;
 i = size - 1;
 while (c < size)
@@ -27,7 +27,6 @@ j--;
 _putchar('\n');
 c++;
 }
-
 if (size <= 0)
 _putchar('\n');
 }
