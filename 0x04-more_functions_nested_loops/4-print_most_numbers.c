5cc2f4d7b7e64fce4716710c7e2ebbd41a7995e2
Gilbert Benimana <112483007+GilbertBenimana@users.noreply.github.com>
2023-03-23 15:55:15 +0200

    Update 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,19 +1,23 @@
 #include "main.h"
 
 /**
- * print_most_numbers - a function that prints the numbers, from 0 to 9.
- * Do not print 2 and 4.
- * followed by a new line.
- * Return: Always 0
+   * more_numbers - a function that prints 10 times the numbers, from 0 to 14,
+   * followed by a new line.
+   * Return: Always 0
  */
-void print_most_numbers(void)
+void more_numbers(void)
 {
-	int i = '0';
+	int i, j = 0;
 
-	for (; i <= '9'; i++)
+	for (; j < 10; j++)
 	{
-		if (i != '2' && i != '4')
-			_putchar(i);
+		for (i = 0; i <= 14; i++)
+		{
+			if (i > 9)
+				_putchar(i / 10 + '0');
+
+			_putchar(i % 10 + '0');
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
