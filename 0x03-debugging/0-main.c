aef306c156d1ce5317afb14bc273f828b1b2995b
Gilbert Benimana <bengil994.bg@gmail.com>
2023-03-23 01:04:12 -0700

    Debugging in C

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include "main.h"
+
+/**
+ * positive_or_negative - positive_or_negative
+ * Description: Print if the number is positive or negative
+ * i: is positive or negative
+ */
+ /* i is positive or negative will be printed*/
+void positive_or_negative(int i)
+{
+/* i is positive */
+if (i > 0)
+{
+printf("%d is positive\n", i);
+}
+/* i is zero */
+else if (i == 0)
+{
+printf("%d is zero\n", i);
+}
+/* i is negative */
+else
+{
+printf("%d is negative\n", i);
+}
+}
