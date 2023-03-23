6b70cc7bc980926a34b804ac6344fc4546538747
Gilbert Benimana <112483007+GilbertBenimana@users.noreply.github.com>
2023-03-23 20:33:17 +0200

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,25 +13,26 @@ int i;
 
 for (i = 1; i <= 100; i++)
 {
-if (i%3 == 0 && i%5 == 0)
+if (i % 3 == 0 && i % 5 == 0)
 {
 printf("FizzBuzz");
 }
-else if (i%3 == 0)
+else if (i % 3 == 0)
 {
 printf("Fizz");
 }
-else if (i%5 == 0)
+else if (i % 5 == 0)
 {
 printf("Buzz");
 }
 else
 {
-printf("%d", i);
+printf(" % d", i);
 }
 if(i < 100)
 {
-putchar(' '); 
+putchar(' ');
 }
 }
+return (0);
 }
