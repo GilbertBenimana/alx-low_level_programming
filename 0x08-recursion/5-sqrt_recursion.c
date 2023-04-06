bd01a1f5e863b131ca2c8c81e65d46da01f75b03
Gilbert Benimana <112483007+GilbertBenimana@users.noreply.github.com>
2023-04-06 20:29:28 +0200

    Update 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -7,10 +7,6 @@
   * Return: Return int.
   */
 
-int _sqrt_recursion(int n)
-{
-return (_sqrt(n, 1));
-}
 int _sqrt(int n, int i)
 {
 int sqrt = i * i;
@@ -19,4 +15,8 @@ return (-1);
 if (sqrt == n)
 return (i);
 return (_sqrt(n, i + 1));
+}
+int _sqrt_recursion(int n)
+{
+return (_sqrt(n, 1));
 }
