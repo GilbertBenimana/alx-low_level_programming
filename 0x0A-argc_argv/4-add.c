89cb7379a02c72f017919f3f9ae408e6b6257b20
Gilbert Benimana <112483007+GilbertBenimana@users.noreply.github.com>
2023-04-11 02:07:38 +0200

    Update 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,63 +1,46 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
- * main -  a program that adds positive numbers
- * @argc: number of arguments
- * @argv: array of arguments
- * Description:  a program that adds positive numbers
- * Return: Return 0 for success and 1 for failure
- */
-
+  * main - Prints the sum of args positive numbers
+  * @argc: argument count
+  * @argv: argument vector
+  *
+  * Return: Always zero
+  */
 int main(int argc, char *argv[])
 {
-int i, sum = 0;
-int r = 0;
-char j;
+int i;
+unsigned int k, sum = 0;
+char *e;
+
 if (argc > 1)
 {
-for (j = '0'; j <= '9'; j++)
-{
-if (j == c)
-{
-isdigit = 1;
-}
-}
 for (i = 1; i < argc; i++)
 {
-sum += atoi(argv[i]);
+e = argv[i];
+
+for (k = 0; k < strlen(e); k++)
+{
+if (e[k] < 48 || e[k] > 57)
+{
+printf("Error\n");
+return (1);
 }
+}
+
+sum += atoi(e);
+e++;
+}
+
 printf("%d\n", sum);
-i = 0;
 }
 else
 {
-printf("Error\n");
-i = 1;
-}
-return (i);
+printf("0\n");
 }
 
-/**
- * _isdigit - Check if c is a digit
- * @c: An input int
- * Description: function return 1 if character is digit
- * and 0 otherwise
- * Return: 1 if is digit or 0 if is not
- */
-
-int _isdigit(int c)
-{
-char i;
-int isdigit = 0;
-
-for (i = '0'; i <= '9'; i++)
-{
-if (i == c)
-{
-isdigit = 1;
-}
-}
-return (isdigit);
-}
+return (0);
 }
