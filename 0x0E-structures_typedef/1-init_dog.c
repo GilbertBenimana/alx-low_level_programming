5c99691097256b0a58943bb5349d2e533a6e6f76
Gilbert Benimana <bengil994.bg@gmail.com>
2023-04-17 08:44:03 -0700

    Structures and typedef

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+/**
+ * init_dog - initializes a variable of type struct dog
+ * @d: parameter of dog type
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ * Description: initializes a variable of type struct dog
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+d->name = name;
+d->age = age;
+d->owner = owner;
+}
+
--- 0x0E-structures_typedef/README.md
+++ 0x0E-structures_typedef/README.md
@@ -1,1 +1,5 @@
-Structures and typedef
+[main b98898f] Structures and typedef
+ 3 files changed, 33 insertions(+)
+ create mode 100644 0x0E-structures_typedef/0-main.c
+ create mode 100644 0x0E-structures_typedef/README.md
+ create mode 100644 0x0E-structures_typedef/dog.h
