cb10962eed0995bed5cc3e7b5bcecedddf13aa45
Gilbert Benimana <112483007+GilbertBenimana@users.noreply.github.com>
2023-04-17 18:30:58 +0200

    Update 1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -14,9 +14,13 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 if (d != NULL)
 {
+d = malloc(sizeof(struct dog));
+if (d != NULL)
+{
 d->name = name;
 d->age = age;
 d->owner = owner;
+}
 }
 }
 
