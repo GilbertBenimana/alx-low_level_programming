0b3508ae4b1c137ce8c051f65aa9ea0f052add89
Gilbert Benimana <112483007+GilbertBenimana@users.noreply.github.com>
2023-04-17 19:45:42 +0200

    Update 3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,7 +1,4 @@
-#ifndef __MACRO_H
-#define __MACRO_H
-#ifndef ABS
-#define ABS(x) (x * ((x > 0) - (x < 0)))
-/*code goes here*/
-#endif /*#ifndef ABS(x)*/
+#ifndef _HEADER_
+#define _HEADER_
+#define ABS(x) ((x) < 0 ? ((x) * -1) : (x))
 #endif
