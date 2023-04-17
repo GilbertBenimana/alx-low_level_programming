4c1338c078c0698da67c5c930f1a81da630d359b
Gilbert Benimana <112483007+GilbertBenimana@users.noreply.github.com>
2023-04-17 19:18:18 +0200

    Update 2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,29 +12,8 @@ void print_dog(struct dog *d)
 {
 if (d != NULL)
 {
-if (d->name != NULL)
-{
-printf("Name: %s\n", d->name);
-}
-if (d->name == NULL)
-{
-printf("Name: (nil)\n");
-}
-if (d->age > 0)
-{
-printf("Age: %.1f\n", d->age);
-}
-if (d->age  <= 0)
-{
-printf("Age: (nil)\n");
-}
-if (d->owner != NULL)
-{
-printf("Owner: %s\n", d->owner);
-}
-if (d->owner == NULL)
-{
-printf("Owner: (nil)\n");
-}
+printf("Name: %s\n", (d->name) ? d->name : "(nil)");
+printf("Age: %f\n", (d->age) ? d->age : 0);
+printf("Owner: %s\n", (d->owner) ? d->owner : "(nil)");
 }
 }
