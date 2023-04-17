675cc25a105dbd0c36307c4dbb3283a5ca20f31b
Gilbert Benimana <112483007+GilbertBenimana@users.noreply.github.com>
2023-04-17 17:05:36 +0200

    Structures and typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,13 +1,13 @@
+#ifndef __DOG_H
+#define __DOG_H
+
 /**
- * dog -a new type struct dog with the following elements
+ * struct dog -a new type struct dog with the following elements
  * @name: dog name
  * @age: dog age
  * @owner: dog owner
  * Description: new type struct dog with the following elements
  */
-
-#ifndef __DOG_H
-#define __DOG_H
 
 struct dog
 {
