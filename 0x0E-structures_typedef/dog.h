b98898f3fed98a9076735b8ff4b967e8f476b00d
Gilbert Benimana <bengil994.bg@gmail.com>
2023-04-17 07:54:15 -0700

    Structures and typedef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+/**
+ * dog -a new type struct dog with the following elements
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ * Description: new type struct dog with the following elements
+ */
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+}
