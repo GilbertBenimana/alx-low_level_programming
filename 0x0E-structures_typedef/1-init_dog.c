488f2f37fca6f3a9dec00dad4ca87d96c3e76212
Gilbert Benimana <112483007+GilbertBenimana@users.noreply.github.com>
2023-04-17 18:25:10 +0200

    Update 1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include "dog.h"
 
 /**
@@ -11,8 +12,11 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+if (d != NULL)
+{
 d->name = name;
 d->age = age;
 d->owner = owner;
+}
 }
 
