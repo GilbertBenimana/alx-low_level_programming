7602620293eebd8afa8d9c57b6b90b6127ce9046
Gilbert Benimana <112483007+GilbertBenimana@users.noreply.github.com>
2023-04-17 18:19:31 +0200

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,4 +16,15 @@ float age;
 char *owner;
 };
 
+/**
+ * dog_t - Typedef for dog structure
+ */
+
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
 #endif
